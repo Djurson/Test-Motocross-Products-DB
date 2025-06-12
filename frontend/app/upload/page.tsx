0088ecfb009c89674b0526fdf4b69c3265cadd29
Emil Djurson <emil@djurson.se>
2025-06-12 16:34:25 +0200

    Lite små ändringar

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -4,14 +4,15 @@ import { ChangeEvent, DragEvent, useState } from "react";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { FileText, FileUp, Info, X } from "lucide-react";
+import { FileText, FileUp, X } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { DropDown } from "@/components/dropdown";
-import { ComboboxDemo } from "@/components/dropdownupload";
+import { tryCatch } from "@/utils/trycatch";
+import axios from "axios";
 
 export default function Page() {
   const [isDragging, setIsDragging] = useState(false);
+  const [category, setCategory] = useState("");
   const [fileToUpload, setFileToUpload] = useState<File | undefined>();
 
   const handleDrop = (e: DragEvent<HTMLDivElement>) => {
@@ -35,7 +36,7 @@ export default function Page() {
 
   async function ButtonClickUpload() {
     if (!fileToUpload) return;
-    await UploadFileToBackend(fileToUpload);
+    await UploadFileToBackend(fileToUpload, category);
   }
 
   return (
@@ -43,7 +44,7 @@ export default function Page() {
       <Card className="w-full max-w-6xl">
         <CardHeader>
           <CardTitle>Ladda upp produkter</CardTitle>
-          <CardDescription>Ladda upp produkter och koppla dem till kategorierer</CardDescription>
+          <CardDescription>Ladda upp produkter och koppla dem till kategorier</CardDescription>
         </CardHeader>
         <CardContent>
           <>
@@ -95,14 +96,10 @@ export default function Page() {
                   </div>
                   <X className="relative w-6 h-6 text-red-600 cursor-pointer -top-6 -right-40 aspect-square" onClick={() => setFileToUpload(undefined)} />
                 </div>
-                <div className="flex justify-center items-center gap-8 w-full">
+                <div className="flex justify-center items-center gap-2 w-full">
                   <div className="grid grid-cols-1 grid-rows-2 w-xs">
-                    <Label>Kategori</Label>
-                    <Input placeholder="Exempelvis fjädring, kolvar etc..." />
-                  </div>
-                  <div className="grid grid-cols-1 grid-rows-2 w-fit">
-                    <Label>Förkortning</Label>
-                    <Input placeholder="KT, DFD, DFZE, TA ..." />
+                    <Label>Huvud kategori</Label>
+                    <Input placeholder="Exempelvis fjädring, kolvar etc..." onChange={(e) => setCategory(e.target.value)} value={category} />
                   </div>
                 </div>
                 <Button size="lg" onClick={ButtonClickUpload}>
@@ -117,4 +114,21 @@ export default function Page() {
   );
 }
 
-async function UploadFileToBackend(file: File) {}
+async function UploadFileToBackend(file: File, rootCategory: string) {
+  const formData = new FormData();
+
+  formData.append("file", file);
+  formData.append("category", rootCategory);
+
+  const { data, error } = await tryCatch(
+    axios.post("http://localhost:8000/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+  );
+
+  if (error !== null) {
+    console.error("Upload error:", error);
+  }
+}
